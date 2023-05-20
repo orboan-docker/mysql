06874c0d41df386bdd11903f0fbe536afe69bc08
Oriol Boix Anfosso <orboan@pluralcamp.com>
2023-05-20 10:03:10 +0000

    updated

--- initdb/fix_user.sh
+++ initdb/fix_user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
   
-user=$NB_USER
+user=$USER
 
 mysql -u root -padmin -e "CREATE USER '$user'@'%' IDENTIFIED BY 'pluralcamp'; GRANT ALL PRIVILEGES ON *.* TO '$user'@'%'; FLUSH PRIVILEGES;";
--- install.sh
+++ install.sh
@@ -14,5 +14,9 @@ chmod +x $bindir/*
 rmdir $dockerdir/$dir/bin
 rm -f $bindir/$0
 
+## Installing mysql cli client
+sudo apt update
+sudo apt install mysql-client-core-8.0 libgbm1 libwayland-server0  -y
+
 exit 0
 
